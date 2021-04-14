1d603b72d5441399ae89f2b583919cc714c793d4
balg <2700278+balg@users.noreply.github.com>
2021-04-14 18:47:54 +0200

    ProgressBar implementation

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,62 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--border-radius": "4px",
+    "--padding": "0px",
+    "--height": "8px",
+  },
+  medium: {
+    "--border-radius": "4px",
+    "--padding": "0px",
+    "--height": "12px",
+  },
+  large: {
+    "--border-radius": "8px",
+    "--padding": "4px",
+    "--height": "24px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper value={value} style={SIZES[size]}>
+      <VisuallyHidden>{`${value} %`}</VisuallyHidden>
+      <Bar value={value} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div.attrs((props) => ({
+  role: "progressbar",
+  "aria-valuemin": "0",
+  "aria-valuemax": "100",
+  "aria-valuenow": props.value || 0,
+}))`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  position: relative;
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  height: var(--height);
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  border-radius: 4px;
+  height: 100%;
+  clip-path: polygon(
+    0 0,
+    ${(props) => props.value + "%"} 0,
+    ${(props) => props.value + "%"} 100%,
+    0 100%,
+    0 0
+  );
+`;
 
 export default ProgressBar;
